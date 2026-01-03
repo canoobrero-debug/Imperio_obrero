667e67691b88afe16232a2663273d071ba371cb7
Obrero <Canoobrero@gmail.com>
2026-01-03 14:09:18 -0400

    SOLUCIÓN: Estructura corregida y Banco Activo

--- app/src/main/java/com/cosmo/empire/bank/GeminiBank.java
+++ app/src/main/java/com/cosmo/empire/bank/GeminiBank.java
@@ -1,10 +1,6 @@
 package com.cosmo.empire.bank;
-
 public class GeminiBank {
-    private static final double IMPERIAL_BALANCE = 100000000000.00;
-    private static final String OWNER_EMAIL = "Canoobrero@gmail.com";
-    
-    public static String getOwner() {
-        return OWNER_EMAIL;
-    }
+    public static final String OWNER = "Rodolfo Obrero";
+    public static final String EMAIL = "Canoobrero@gmail.com";
+    public static final double FONDOS = 100000000000.00;
 }
